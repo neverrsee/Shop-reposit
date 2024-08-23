c9994dd61fd4d98e54139ccb28e0aa841d999b0a
Eric <megalovania4gg@gmail.com>
2024-08-23 19:35:46 +0300

    оптимизация проекта

--- Core/urls.py
+++ Core/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
 from .views import *
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('signup/', signup, name='singup'),
     path('signin/', signin, name='singin'),
     path('signout/', signout, name='singout'),
     path('profile/', profile, name='profile'),
+    path('shop/', include('Shop.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
